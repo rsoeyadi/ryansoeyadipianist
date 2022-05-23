5edf157209509a0e71f1485568f3cf91e9c8d2b7
Ryan Soeyadi <rs4163@columbia.edu>
2022-05-23 12:13:47 -0400

    image styling

--- src/components/Aboutimg.js
+++ src/components/Aboutimg.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
-import styled, { createGlobalStyle } from "styled-components";
 import { StaticImage } from "gatsby-plugin-image";
-import {StaticImageStyle} from "./Aboutimg.module.css"
+import { StaticImageStyle } from "./Aboutimg.module.css";
+import styled, { createGlobalStyle } from "styled-components";
 
+const StaticImageWrapper = styled.div`
+  padding: 1.5em;
+`
 const Aboutimg = () => {
-  
   return (
-    <StaticImage
-      src="../images/about-img.jpg"
-      alt="Ryan Soeyadi Headshot"
-      className={StaticImageStyle}/>
+    <StaticImageWrapper>
+      <StaticImage
+        src="../images/about-img.jpg"
+        alt="Ryan Soeyadi Headshot"
+        className={StaticImageStyle}
+      />
+    </StaticImageWrapper>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ const index = () => {
   return (
     <>
       <Layout />
+      Biography
       <Aboutimg />
       
       
