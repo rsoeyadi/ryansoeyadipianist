7978a1294e1630e3f154499624bb2602fcd066e1
Ryan Soeyadi <rs4163@columbia.edu>
2022-05-25 14:35:26 -0400

    raw text now parsed

--- src/pages/blog/{contentfulBlogPost.slug}.js
+++ src/pages/blog/{contentfulBlogPost.slug}.js
@@ -2,30 +2,43 @@ import React from "react";
 import Layout from "../../components/Layout";
 import Header from "../../components/Heading";
 import { graphql } from "gatsby";
+import { renderRichText } from "gatsby-source-contentful/rich-text";
+import { INLINES, BLOCKS, MARKS } from "@contentful/rich-text-types";
+
+const options = {
+  renderMark: {
+    [MARKS.BOLD]: (text) => <b className="font-bold">{text}</b>,
+  },
+  renderNode: {
+    [INLINES.HYPERLINK]: (node, children) => {
+      const { uri } = node.data;
+      return (
+        <a href={uri} className="underline">
+          {children}
+        </a>
+      );
+    },
+    [BLOCKS.HEADING_2]: (node, children) => {
+      return <h2>{children}</h2>;
+    },
+  },
+};
 
 const BlogPost = ({ location, data }) => {
   var node;
   for (let i = 0; i < data.allContentfulBlogPost.edges.length; i++) {
-    if (
-      data.allContentfulBlogPost.edges[i].node.slug === location.state.passSlug
-    ) {
+    if (data.allContentfulBlogPost.edges[i].node.slug === location.state.passSlug) {
       node = data.allContentfulBlogPost.edges[i].node;
       break;
     }
   }
 
-  console.log(node);
-
   return (
     <Layout>
       <h1>{node.title}</h1>
+      <p>{node.date}</p>
       <p>{node.slug}</p>
-      <h1>{node.title}</h1>
-      <p>{node.slug}</p>
-      <h1>{node.title}</h1>
-      <p>{node.slug}</p>
-      <h1>{node.title}</h1>
-      <p>{node.slug}</p>
+      <p>{renderRichText(node.content, options)}</p>
     </Layout>
   );
 };
@@ -43,6 +56,7 @@ export const query = graphql`
           }
           content {
             raw
+            
           }
         }
       }
