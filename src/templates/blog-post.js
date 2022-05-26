17e37af3a1b5b9d1dab893fb8398188e757d9f1c
Ryan Soeyadi <rs4163@columbia.edu>
2022-05-26 14:29:26 -0400

    templated blog pages

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { graphql, Link } from "gatsby";
 
-import Layout from "../../components/layout";
 import Img from "gatsby-image";
 
 export const query = graphql`
